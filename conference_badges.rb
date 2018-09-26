240315754859ab069992d3995ba126fad8b19dcd
Sydney Grant <sg3957@nyu.edu>
2018-09-26 15:58:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  count = 0 
+  while count < arr.length 
+  badge_maker(arr[count])
+  count++
+end
+end
+
+def assign_rooms(arr)
+  
+end
